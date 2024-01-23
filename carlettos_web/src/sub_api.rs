7abfb9900795fa42b42485aad94df07573ac0f32
Carlettos <kako3000@gmail.com>
2024-01-23 14:45:19 -0300

    Update BASE_URL in sub_api.rs

--- carlettos_web/src/sub_api.rs
+++ carlettos_web/src/sub_api.rs
@@ -3,7 +3,7 @@ use reqwasm::{http::Request, Error};
 
 use crate::models::{AffectedRows, Task};
 
-const BASE_URL: &str = "http://bore.pub:60083";
+const BASE_URL: &str = "http://localhost:8080";
 
 pub async fn get_chess_game() -> Result<Board, Error> {
     Request::get(&format!("{BASE_URL}/chess"))
