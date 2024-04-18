384d654fe30079c8911d6ab2ff7dd67d4b979f2a
Carlettos <kako3000@gmail.com>
2024-04-18 16:43:07 -0400

    a

--- carlettos_api/run_prod.sh
+++ carlettos_api/run_prod.sh
@@ -1,3 +1,3 @@
 export DB_IP="ws://64.23.184.29:8000"
-export ROCKET_TLS={certs="/etc/letsencrypt/live/scirustic.cl/fullchain.pem",key="/etc/letsencrypt/live/scirustic.cl/privkey.pem"}
+export ROCKET_TLS="{certs="/etc/letsencrypt/live/scirustic.cl/fullchain.pem",key="/etc/letsencrypt/live/scirustic.cl/privkey.pem"}"
 cargo r -r
